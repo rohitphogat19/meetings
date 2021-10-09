2893aa220ff9f5ed48a85a1e2ce605d2466dd470
Rohit <rohit.kumar@bigsteptech.com>
2021-10-09 16:23:00 +0530

    Updated Api Paths

--- app.js
+++ app.js
@@ -12,14 +12,14 @@ dotenv.config();
 
 app.use(cors());
 app.use(express.json());
-app.use("/v1/users", userRoutes);
-app.use("/v1/auth", authRoutes);
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/auth", authRoutes);
 
-app.get('/.well-known/apple-app-site-association', function (request, response) {
+app.get('/api/.well-known/apple-app-site-association', function (request, response) {
     response.sendFile(__dirname + '/apple-app-site-association.json');
 });
 
-app.get("/home",
+app.get("/api/home",
     function (req, res) {
         return res.json("Hello to home")
     })
